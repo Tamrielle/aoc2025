6a123d75eed627153edc1f20913de8131e663360
Tamrielle <lanner.manuela@gmail.com>
2025-12-03 12:29:22 +0100

    Prepared for day4

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,18 @@
+import utils.InputUtils
+
+class Day4 {
+
+    val input = InputUtils().readLines(4)
+
+    fun solvePart1(): Long {
+        return 0L
+    }
+
+
+    fun solvePart2(): Long {
+        return 0L
+    }
+
+
+}
+
---
+++ src/test/kotlin/TestDay4.kt
@@ -0,0 +1,19 @@
+import kotlin.test.Test
+
+class TestDay4 {
+    private val day4 = Day4()
+
+    @Test
+    fun testSolvePart1() {
+        val solution = day4.solvePart1()
+        println(solution)
+    }
+
+    @Test
+    fun testSolvePart2() {
+        val solution = day4.solvePart2()
+        println(solution)
+    }
+
+
+}
